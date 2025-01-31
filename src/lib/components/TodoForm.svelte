c6edb985da25873c16631d82370dc89f87c10b8f
Carlos Henrique <hencarlosdv@gmail.com>
2025-01-31 13:14:07 -0300

    ref: ajusta cores

--- src/lib/components/TodoForm.svelte
+++ src/lib/components/TodoForm.svelte
@@ -12,7 +12,7 @@
   <label class="sr-only" for="addTodo">
   </label>
   <div class="flex gap-4 w-full">
-    <input name="addTodo" bind:value={newTodo} class="bg-neutral-50 text-neutral-200 w-5/6 py-4 rounded-md px-4 placeholder:text-neutral-500" type="text" placeholder="Add todo">
-    <button class="bg-neutral-50 w-1/6 text-zinc-700 rounded-md cursor-pointer">Add todo</button>
+    <input name="addTodo" bind:value={newTodo} class="bg-neutral-50 w-5/6 py-4 rounded-md px-4 text-zinc-800" type="text" placeholder="Add todo">
+    <button class="bg-neutral-50 w-1/6 text-zinc-700 rounded-md cursor-pointer hover:bg-neutral-50/80 transition-all">Add todo</button>
   </div>
 </form>
